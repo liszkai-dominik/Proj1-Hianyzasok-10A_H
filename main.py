53c026238e345bab58c15dea4c3db48125b8243e
magyar-matyas <magyar.matyas@students.jedlik.eu>
2023-02-14 09:35:38 +0100

    txt beolvasása 2 kész Fixes #15

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-from Megoldas import Megoldas
-
-
 def main() -> None:
-mo: Megoldas = Megoldas('naplo.txt')
+# 1. feladat: Beolvasás
+    naplo = {}
+    with open('naplo.txt', 'r', encoding='utf-8') as file:
+        for sor in file:
+            if sor[0] == "#":
+                egyNap = sor.strip().split(" ")
+                egyNapKulcs = egyNap[1] + "." + egyNap[2]
+                naplo[egyNapKulcs] = []
+            else:
+                egyHianyzo = sor.strip().split(" ")
+                naplo[egyNapKulcs].append([egyHianyzo[0] + " " + egyHianyzo[1], egyHianyzo[2]])
