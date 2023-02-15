2d7258bc987632b19639dc8459b404b65c76afa9
Liszkai Dominik <liszkai.dominik@students.jedlik.eu>
2023-02-15 08:49:23 +0100

    2. feladat megoldásának befejezése  -kész

--- Megoldas.py
+++ Megoldas.py
@@ -7,16 +7,9 @@ class Megoldas:
     def __init__(self, állomány_neve: str) -> None:
         self._hianyzasok = []
         with open(állomány_neve, 'r', encoding='utf-8') as file:
-            for értékek in file.read().splitlines():
-                if értékek[0] == "#":
-                    h_n = értékek.strip().split(" ")
-                    dátum = h_n[1] + "." + h_n[2]
+            dátum: str = ''
+            for sor in file.read().splitlines():
+                if sor[0] == "#":
+                    dátum = sor
                 else:
-                    while értékek[0] != "#":
-                        sor: str = ""
-                        hiányzó = értékek.strip().split(" ")
-                        teljes_név = hiányzó[0] + ";" + hiányzó[1]
-                        hiányzás = hiányzó[2]
-                        sor: str = dátum + ";" + teljes_név + ";" + hiányzás
-                        self.hianyzasok.append(Hianyzasok(sor))
-        
+                    self.hianyzasok.append(Hianyzasok(dátum, sor))
