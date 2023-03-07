eb3843fb01df9d511fef75fce75f382107ed3c10
liszkai-dominik <liszkai.dominik@students.jedlik.eu>
2023-03-07 17:44:43 +0100

    Hétnapja függvény javítása Fixes #52

--- Seged.py
+++ Seged.py
@@ -2,6 +2,6 @@ class Seged:
     @staticmethod
     def hetnapja(hónap: int, nap: int) -> str:
         napok_neve = ["vasarnap", "hétfő", "kedd", "szerda", "csütörtök", "péntek", "szombat"]
-        napok_száma = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 335]
+        napok_száma = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
         nap_sorszam = (napok_száma[hónap - 1] + nap) % 7
         return napok_neve[nap_sorszam]
